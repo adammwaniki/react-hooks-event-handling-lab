92419738e137f920204b7d41e49258183acc3fe7
Adam Mwaniki <adamndegwa@protonmail.com>
2024-04-26 18:02:23 +0300

    Creates a keypad and eyes on me component

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,17 @@
 // Code Keypad Component Here
 
+import { mockComponent } from "react-dom/test-utils";
+
 function Keypad (){
+    function handleChange(event){
+        console.log("Entering password...")
+        //console.log(event.target.value) //this will return the password itself i.e "123"
+    }
     return (
-        <div></div>
-    )
+        <div>
+            <input type="password" placeholder="Enter your password" onChange={handleChange} />
+        </div>
+    );
 }
 
 export default Keypad;
